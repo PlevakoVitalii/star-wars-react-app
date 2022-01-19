10bfeac609fff80e3816e335382e28a8428b2853
PlevakoVitalii <plevakovl2102@gmail.com>
2022-01-19 17:35:47 +0200

    docs: update readme with additional information

--- src/components/sw-components/starship-details.jsx
+++ src/components/sw-components/starship-details.jsx
@@ -4,28 +4,16 @@ import ItemDetails, { Record } from '../item-details';
 
 import { SwapiServiceConsumer } from '../swapi-service-context';
 
-import { useParams} from "react-router-dom";
-// В етом файле создали компоненты которые
-// будут отображать детали для PersonDetails
+import { useParams } from "react-router-dom";
 
 const StarshipDetails = () => {
-  
-// //Ф-ия благодаря хуку useParams() получит id из URL
-// // и передасте ее в  <StarshipDetails itemId={id} />
 
   let { id } = useParams();
   return (
-//Для передачи контекста (swapiService) можно исользовать props, Provider-Consumer или HOC
-//В єто компоненте мі используем Provider-Consumer 
-    //Испозуем SwapiServiceConsumer для получения swapiService,
-    // переданного через контекст из app.js из SwapiServiceProvider на
-    //Дале деструктруируем из  swapiService нужные ф-ии {getPlanet, getPlanetImage}
-    //По правилам SwapiServiceConsumer должен принимать ф-ию
- 
 
     <SwapiServiceConsumer>
       {
-        ({ getStarship, getStarshipImage}) => {
+        ({ getStarship, getStarshipImage }) => {
           return (
             <ItemDetails
               itemId={id}
